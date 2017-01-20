3c2eff2019a98435dcec3a7d8949f5d8aea40ef0
강희우 <heewoo.kang@ysmcorp.com>
2017-01-20 16:52:57 +0900

    scala-spark project test

--- build.sbt
+++ build.sbt
@@ -2,7 +2,8 @@ name := "spark_s"
 
 version := "1.0"
 
-scalaVersion := "2.11.8"
-
-libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.1.0"
-
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.0.0"
+libraryDependencies += "org.apache.spark" % "spark-mllib_2.11" % "2.0.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.0.0"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.0.0"
+libraryDependencies += "org.apache.spark" % "spark-graphx_2.11" % "2.0.0"
---
+++ src/main/scala/com/test/spark/develop.scala
@@ -0,0 +1,54 @@
+package com.test.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+
+
+object develop {
+
+  val conf = new SparkConf().setMaster("local").setAppName("heewoo")
+  val sc = new SparkContext(conf)
+
+  val input = sc.textFile("/Users/yellomobile/Desktop/sitemap-1.xml")
+
+  def textSave(): Unit ={
+    val word = input.flatMap(line => line.split(" "))
+    val count = word.map(word => (word,1)).reduceByKey{case (x,y) => x+y}
+    count.saveAsTextFile("/Users/yellomobile/Desktop/sitemap-heewoo.xml")
+  }
+
+  def textCount(): Unit ={
+    println(input.count())
+  }
+
+
+  def createRDD(): Unit ={
+    val lines = sc.parallelize(List("heewoo","test","spark"))
+  }
+
+  def filter(): Unit ={
+    val url = input.filter(x=>x.contains("feed"))
+      println(url.first())
+      println(url.count())
+  }
+
+  def take(): Unit ={
+    input.take(5).foreach(println)
+  }
+
+
+  def flatMap(): Unit ={
+    val lines = sc.parallelize(List("heewoo world" , "hello" , "world"))
+    val word = lines.flatMap(line => line.split(" "))
+
+    println(word.first())
+  }
+
+  def main(args: Array[String]): Unit = {
+    take()
+  }
+
+
+
+}
